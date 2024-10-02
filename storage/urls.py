cd08dab3acdce3a43adba7df93c92a4cd647b452
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-10-01 23:19:00 -0700

    feat(templates,views): templates for different search options

--- storage/urls.py
+++ storage/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
 from . import views
+from .views import simple_view
 
 urlpatterns = [
-    path('', views.index, name='storage-index'),
-    path('add', views.add, name='storage-add'),
-    path('add-part', views.add_part, name='storage-add-part'),
-    path('search', views.search, name='storage-search'),
-    path('search/autopart', views.search_autopart, name='storage-search-autopart'),
-    path('search/location', views.search_location, name='storage-search-location'),
-    path('search/stock', views.search_stock, name='storage-search-stock'),
+    path('', simple_view('storage/index.html'), name='storage-index'),
+    path('add', simple_view('storage/add.html'), name='storage-add'),
+    path('add-part', simple_view('storage/add-part.html'), name='storage-add-part'),
+    path('search', simple_view('storage/search.html'), name='storage-search'),
+    path('search/autopart', simple_view('storage/search-autopart.html'), name='storage-search-autopart'),
+    path('search/autopart/results', views.search_autopart_results, name='storage-search-autopart-results'),
+    path('search/location', simple_view('storage/search-location.html'), name='storage-search-location'),
+    path('search/location/results', views.search_location_results, name='storage-search-location-results'),
+    path('search/stock', simple_view('storage/search-stock.html'), name='storage-search-stock'),
+    path('search/stock/results', views.search_stock_results, name='storage-search-stock-results'),
 ]
--- storage/views.py
+++ storage/views.py
@@ -1,38 +1,29 @@
+from collections.abc import Callable
+
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
+def simple_view(template_name: str) -> Callable[[HttpRequest], HttpResponse]:
+    @login_required
+    def inner(request: HttpRequest) -> HttpResponse:
+        return render(request, template_name)
+
+    return inner
+
+
+# TODO: Actually implement the search features
 @login_required
-def index(request: HttpRequest) -> HttpResponse:
-    return render(request, 'storage/index.html')
-
-
-@login_required
-def add(request: HttpRequest) -> HttpResponse:
-    return render(request, 'storage/add.html')
-
-
-@login_required
-def add_part(request: HttpRequest) -> HttpResponse:
-    return render(request, 'storage/add-part.html')
-
-
-@login_required
-def search(request: HttpRequest) -> HttpResponse:
-    return render(request, 'storage/search.html')
+def search_autopart_results(request: HttpRequest) -> HttpResponse:
+    return render(request, 'storage/results.html')
 
 
 @login_required
-def search_autopart(request: HttpRequest) -> HttpResponse:
-    return render(request, 'storage/search-autopart.html')
+def search_location_results(request: HttpRequest) -> HttpResponse:
+    return render(request, 'storage/results.html')
 
 
 @login_required
-def search_location(request: HttpRequest) -> HttpResponse:
-    return render(request, 'storage/search-location.html')
-
-
-@login_required
-def search_stock(request: HttpRequest) -> HttpResponse:
-    return render(request, 'storage/search-stock.html')
+def search_stock_results(request: HttpRequest) -> HttpResponse:
+    return render(request, 'storage/results.html')
