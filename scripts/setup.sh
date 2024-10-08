4f005120099822551a0aa3449c156ce8bd236984
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-10-08 16:33:38 -0700

    feat(scripts): setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,6 @@
+python manage.py makemigrations
+python manage.py makemigrations core
+python manage.py makemigrations sales
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py collectstatic
